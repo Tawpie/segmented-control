f5ba6fa5eece636e2b4829bde7ccaca00f7b5e3e
Kim Hansen <khansen@2morrowinc.com>
2017-07-13 10:02:23 -0700

    use onClick instead of onChange

--- src/SegmentedControl.js
+++ src/SegmentedControl.js
@@ -1,63 +1,83 @@
-import React, { PropTypes } from 'react'
-import _ from 'lodash'
+'use strict';
 
-import './SegmentedControl.css'
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-const SegmentedControl = React.createClass({
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+require('./SegmentedControl.css');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var SegmentedControl = _react2.default.createClass({
+  displayName: 'SegmentedControl',
+
   propTypes: {
-    name: PropTypes.string.isRequired,
-    options: PropTypes.array.isRequired,
-    style: PropTypes.object,
-    setValue: PropTypes.func,
+    name: _react.PropTypes.string.isRequired,
+    options: _react.PropTypes.array.isRequired,
+    style: _react.PropTypes.object,
+    setValue: _react.PropTypes.func
   },
 
-  componentWillMount() {
-    const defaultOption = _.find(this.props.options, {default: true})
-    this.setValue(defaultOption.value)
+  componentWillMount: function componentWillMount() {
+    var defaultOption = _lodash2.default.find(this.props.options, { default: true });
+    this.setValue(defaultOption.value);
   },
-
-  setValue(val) {
-    this.props.setValue && this.props.setValue(val)
+  setValue: function setValue(val) {
+    this.props.setValue && this.props.setValue(val);
   },
+  render: function render() {
+    var _this = this;
 
-  render() {
-    const getId = option => this.props.name + option.value
+    var getId = function getId(option) {
+      return _this.props.name + option.value;
+    };
 
-    const defaultStyle = {
+    var defaultStyle = {
       width: '100%'
-    }
+    };
 
-    const style = _.extend(defaultStyle, this.props.style)
+    var style = _lodash2.default.extend(defaultStyle, this.props.style);
 
-    return (
-      <div
-        className="segmented-control"
-        style={style}
-        >
-        {this.props.options.map(option => (
-          <input
-            key={getId(option)}
-            type="radio"
-            name={this.props.name}
-            id={getId(option)}
-            onChange={() => this.setValue(option.value)}
-            defaultChecked={option.default}
-            disabled={option.disabled}
-            />
-          ))
-        }
-        {this.props.options.map(option => (
-          <label
-            key={getId(option)}
-            htmlFor={getId(option)}
-            data-value={option.label}
-            >
-            {option.label}
-          </label>
-        ))}
-      </div>
+    return _react2.default.createElement(
+      'div',
+      {
+        className: 'segmented-control',
+        style: style
+      },
+      this.props.options.map(function (option) {
+        return _react2.default.createElement('input', {
+          key: getId(option),
+          type: 'radio',
+          name: _this.props.name,
+          id: getId(option),
+          onClick: function onChange() {
+            return _this.setValue(option.value);
+          },
+          defaultChecked: option.default,
+          disabled: option.disabled
+        });
+      }),
+      this.props.options.map(function (option) {
+        return _react2.default.createElement(
+          'label',
+          {
+            key: getId(option),
+            htmlFor: getId(option),
+            'data-value': option.label
+          },
+          option.label
+        );
+      })
     );
   }
 });
 
-export default SegmentedControl;
+exports.default = SegmentedControl;
